44d2a613579aef1b9d3ffd682c17cdd9271321dd
Matthew F Leader <mleader@redhat.com>
2024-01-30 09:30:46 -0500

    set label strings in message label test

--- message_label_test.go
+++ message_label_test.go
@@ -8,16 +8,18 @@ import (
 
 func TestMessageLabels(t *testing.T) {
 	var l log.Labels = map[string]string{}
+	labelStrEmpty := l.String()
 	if l.String() != "" {
-		t.Fatalf("Incorrect label string: %s", l.String())
+		t.Fatalf("Incorrect label string: %s", labelStrEmpty)
 	}
 	l = map[string]string{"foo": "bar"}
-	if l.String() != "foo=bar" {
-		t.Fatalf("Incorrect label string: %s", l.String())
+	labelStr1Key := l.String()
+	if labelStr1Key != "foo=bar" {
+		t.Fatalf("Incorrect label string: %s", labelStr1Key)
 	}
 	l = map[string]string{"foo": "bar", "baz": "Hello world!"}
-	labelStr := l.String()
-	if labelStr != "foo=bar;baz=Hello world!" && labelStr != "baz=Hello world!;foo=bar" {
-		t.Fatalf("Incorrect label string 3: %s", l.String())
+	labelStr2Keys := l.String()
+	if labelStr2Keys != "foo=bar;baz=Hello world!" && labelStr2Keys != "baz=Hello world!;foo=bar" {
+		t.Fatalf("Incorrect label string 3: %s", labelStr2Keys)
 	}
 }
