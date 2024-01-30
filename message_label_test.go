d9e3c8f34c0cf2d122b12b76e4992509feb13b3f
Matthew F Leader <mleader@redhat.com>
2024-01-29 19:15:43 -0500

    fix test

--- message_label_test.go
+++ message_label_test.go
@@ -1,22 +1,23 @@
 package log_test
 
 import (
-	"testing"
+    "testing"
 
-	"go.arcalot.io/log/v2"
+    "go.arcalot.io/log/v2"
 )
 
 func TestMessageLabels(t *testing.T) {
-	var l log.Labels = map[string]string{}
-	if l.String() != "" {
-		t.Fatalf("Incorrect label string: %s", l.String())
-	}
-	l = map[string]string{"foo": "bar"}
-	if l.String() != "foo=bar" {
-		t.Fatalf("Incorrect label string: %s", l.String())
-	}
-	l = map[string]string{"foo": "bar", "baz": "Hello world!"}
-	if l.String() != "foo=bar;baz=Hello world!" && l.String() != "baz=Hello world!;foo=bar" {
-		t.Fatalf("Incorrect label string: %s", l.String())
-	}
+    var l log.Labels = map[string]string{}
+    if l.String() != "" {
+        t.Fatalf("Incorrect label string: %s", l.String())
+    }
+    l = map[string]string{"foo": "bar"}
+    if l.String() != "foo=bar" {
+        t.Fatalf("Incorrect label string: %s", l.String())
+    }
+    l = map[string]string{"foo": "bar", "baz": "Hello world!"}
+    labelStr := l.String()
+    if labelStr != "foo=bar;baz=Hello world!" && labelStr != "baz=Hello world!;foo=bar" {
+        t.Fatalf("Incorrect label string 3: %s", l.String())
+    }
 }
